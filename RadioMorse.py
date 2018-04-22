f908c40b70dc7ada3dcd879a075a9d55ad5cc740
Btdyaya <billytdavies@gmail.com>
2018-04-22 09:13:00 +0100

    Add morse

---
+++ RadioMorse.py
@@ -0,0 +1,15 @@
+from microbit import *
+import radio
+
+radio.on()
+radio.config(channel = 10)
+while True:
+    if button_a.was_pressed():
+        radio.send(".")
+    elif button_b.was_pressed():
+        radio.send("-")
+    incoming = radio.receive()
+    
+    if incoming is not None:
+        display.scroll(str(incoming))
+
